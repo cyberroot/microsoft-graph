593daa4ac176f8058feaf7f973ba65381d73d767
Karthik <j.sskarthik@gmail.com>
2017-01-10 22:56:06 +0530

    added email

--- lib/omniauth/strategies/microsoft_v2_auth.rb
+++ lib/omniauth/strategies/microsoft_v2_auth.rb
@@ -17,14 +17,25 @@ module OmniAuth
 
       uid { raw_info["id"] }
 
+      info do
+        {
+          'email' => raw_info["mail"],
+          'first_name' => raw_info["givenName"],
+          'last_name' => raw_info["surname"],
+          'name' => [raw_info["givenName"], raw_info["surname"]].join(' '),
+          'nickname' => raw_info["displayName"],
+        }
+      end
+
       extra do
         {
-          'raw_info' => raw_info
+          'raw_info' => raw_info,
+          'params' => access_token.params
         }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('https://graph.microsoft.com/v1.0/me').parsed
+        @raw_info ||= access_token.get(authorize_params.resource + 'v1.0/me').parsed
       end
 
       def authorize_params
